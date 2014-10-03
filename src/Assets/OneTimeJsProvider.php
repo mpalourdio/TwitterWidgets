a3b2cb08a366fcee1b7a2d3abf299730fdfcc8e0
mpalourdio <mpalourdio@gmail.com>
2014-10-03 09:32:21 +0200

    Do not concat <script> tags to the js code

--- src/Assets/OneTimeJsProvider.php
+++ src/Assets/OneTimeJsProvider.php
@@ -20,13 +20,13 @@ class OneTimeJsProvider
      */
     public function getOneTimeWidgetJs()
     {
-        return '<script>window.twttr = (function (d, s, id) {
-                  var t, js, fjs = d.getElementsByTagName(s)[0];
-                  if (d.getElementById(id)) return;
-                  js = d.createElement(s); js.id = id;
-                  js.src= "https://platform.twitter.com/widgets.js";
-                  fjs.parentNode.insertBefore(js, fjs);
-                  return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
-                }(document, "script", "twitter-wjs"));</script>';
+        return 'window.twttr = (function (d, s, id) {
+                    var t, js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src= "https://platform.twitter.com/widgets.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                    return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
+                }(document, "script", "twitter-wjs"));';
     }
 }
